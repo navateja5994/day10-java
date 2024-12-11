9ab6285608ea4389115e95a3bf3177127d12715f
NAVATEJA <navatejar80@gmail.com>
2024-12-11 14:12:59 +0530

    Add files via upload

---
+++ this.java
@@ -0,0 +1,15 @@
+class Main{
+    void display()
+    {
+        System.out.println("Hello World");
+    }
+    void dis1()
+    {
+        this.display();
+        }
+        public static void main(String args[])
+        {
+            Main o=new Main();
+            o.dis1();
+        }
+}
